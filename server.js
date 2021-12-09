70eeb4f7608704a4194ab33a3f4d2ed100cdb59f
wwbarton <wbartonw5@gmail.com>
2021-12-09 09:29:50 -0700

    file restucture, escpecially for routes-store.js deleted

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 const express = require("express");
-const htmlRoutes = require("./routes/htmlRoutes");
-const apiRoutes = require("./routes/apiRoutes");
+const fs = require("fs");
+const path = require("path");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-//middleware
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.json());
+app.use(express.static(__dirname));
 
-app.use("/", htmlRoutes);
-app.use("/", apiRoutes);
-
-app.listen(PORT, () => {
-  console.log(`API Server is listening on ${PORT}`);
+require("./routes/routes")(app);
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
 });
